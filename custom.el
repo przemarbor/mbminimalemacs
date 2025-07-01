aa2c033e47e789121a1fd4ae8e214f35ae4016a4
przemarbor <marbor@prz.edu.pl>
2025-07-01 21:34:31 +0200

    Dodany auctex/cdlatex - Emacs działa poprawnie

--- custom.el
+++ custom.el
@@ -5,16 +5,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(aggressive-indent auto-complete berry-theme buffer-move calfw calfw-cal
-                       calfw-org cape compile-angel corfu easysession ein
+   '(aggressive-indent auctex auto-complete buffer-move calfw calfw-cal calfw-org
+                       cape cdlatex compile-angel corfu easysession ein
                        embark-consult engrave-faces harpoon helpful
                        highlight-defined htmlize jupyter magit marginalia
                        matlab-mode orderless org-contrib
                        org-special-block-extras outline-indent ox-ipynb
-                       package-lint page-break-lines paredit plantuml-mode
-                       polish-holidays pyvenv rainbow-delimiters scad-mode
-                       smart-shift toc-org undo-fu undo-fu-session vertico
-                       yaml-mode)))
+                       package-lint page-break-lines paredit pdf-tools
+                       plantuml-mode polish-holidays preview pyvenv
+                       rainbow-delimiters scad-mode smart-shift toc-org undo-fu
+                       undo-fu-session vertico yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
